0c6f690aaa00814c0829d5b30456017efa913691
ltsbryant1125 <bl629@drexel.edu>
2023-03-01 22:37:23 -0500

    Reconfigured project for final interface

---
+++ idm362-bl629/ViewControllerMain.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerMain.swift
+//  idm362-bl629
+//
+//  Created by Bryant Lu on 3/1/23.
+//
+
+import UIKit
+
+class ViewControllerMain: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
